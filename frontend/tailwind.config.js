69a0c6c9ebc407c1286954f18d2f3be93f4085b3
Janaina Tannus <61756665+jmtannus@users.noreply.github.com>
2024-05-29 03:02:59 +0000

    escolhendo theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#E83667",
+        primaryDark: "#BB2C51",
+        secondary: "#21D489",
+        dark: "#303030",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        cursive: ["Ephesis", "cursive"],
+      },
+      container: {
+        center: true,
+        padding: {
+          default: "1rem",
+          sm: "2rem",
+          lg: '3rem',
+          xl: '4rem',
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
