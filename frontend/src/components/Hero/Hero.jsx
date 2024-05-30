fbd9de7d8a55309824423a29fa6a387e62e4e057
Janaina <jmtannus@gmail.com>
2024-05-30 01:16:59 -0300

    Hero page responsivo s-uando Tailwind

---
+++ frontend/src/components/Hero/Hero.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import HeroImg from "./../../assets/hero.png";
+import HeroBg from "./../../assets/heroBg.png";
+import PrimaryButton from "../Shared/PrimaryButton";
+
+const BgStyle = {
+  backgroundImage: `url(${HeroBg})`,
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "center",
+  backgroundSize: "cover",
+  width: "100%",
+  height: "100%",
+};
+
+const Hero = () => {
+  return (
+    <>
+      <div style={BgStyle} className="relative z-[-1]">
+        <div className="container py-16 sm:py-0">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 place-items-center min-h-[600px]">
+            {/* text-content section */}
+            <div className="space-y-7 text-slate-950 order-2 sm:order-1">
+              <h1 className="text-5xl">
+                {" "}
+                <span className="text-secondary font-cursive text-7xl">
+                  Jot
+                </span>{" "}
+                down the{" "}
+                <span className="text-secondary font-cursive text-7xl">
+                  Joy
+                </span>{" "}
+                moments
+              </h1>
+              <p className="lg:pr-64 text-slate-950 text-lg">
+                Registre os momentos que te fizeram sorrir hoje e sorria
+                quando relembrá-los.
+              </p>
+
+              {/* button section */}
+              <div>
+                <PrimaryButton />
+              </div>
+            </div>
+            {/* image section */}
+            <div className="relative z-30 order-1 sm:order-2">
+              <img
+                src={HeroImg}
+                alt=""
+                className="w-full sm:scale-125 sm:translate-y-16"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
---
+++ frontend/src/components/Shared/PrimaryButton.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { FaArrowRight } from "react-icons/fa";
+
+const PrimaryButton = () => {
+  return (
+    <>
+      <div className="flex items-center group">
+        <button className="bg-primary h-[40px] text-white px-3 py-2">
+          O que fez você feliz?
+        </button>
+        <FaArrowRight className="inline-block group-hover:!translate-x-2 duration-200 p-2 text-base h-[40px] w-[40px] bg-primaryDark text-white" />
+      </div>
+    </>
+  );
+};
+
+export default PrimaryButton;
