f3663acca8eeae695881d2d17a94a354fab01612
Janaina <jmtannus@gmail.com>
2024-05-30 15:52:09 -0300

    apagando imagens, hero concluida

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -17,16 +17,16 @@ const Hero = () => {
     <>
       <div style={BgStyle} className="relative z-[-1]">
         <div className="container py-16 sm:py-0">
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 place-items-center min-h-[800px]">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 place-items-center min-h-[600px]">
             {/* text-content section */}
-            <div className="space-y-7 text-slate-950 order-2 sm:order-1">
+            <div className="space-y-7 text-dark order-2 sm:order-1">
               <h1 className="text-5xl">
                 {" "}
-                <span className="text-secondary font-cursive text-7xl">
+                <span className="text-secondary font-cursive text-8xl">
                   Jot
                 </span>{" "}
                 down the{" "}
-                <span className="text-secondary font-cursive text-7xl">
+                <span className="text-secondary font-cursive text-8xl ">
                   Joy
                 </span>{" "}
                 moments
