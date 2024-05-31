e60a70f306ac048fe9d2982cfcac70b9d901cb2c
Janaina <jmtannus@gmail.com>
2024-05-31 11:59:12 -0300

    Benefits

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Banner from "./components/Banner/Banner";
+import Benefits from "./components/Benefits/Benefits";
 
 const App = () => {
   return (
-  <div
-   className="overflow-x-hidden">
-   <Navbar />   
-   <Hero />
-   <Banner />
-   </div>
+    <div className="overflow-x-hidden">
+      <Navbar />
+      <Hero />
+      <Banner />
+      <Benefits />
+    </div>
   );
 };
 
---
+++ frontend/src/components/Benefits/Benefits.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import { FaHeartbeat, FaKissWinkHeart, FaLightbulb, FaGift } from "react-icons/fa";
+
+const Benefits = () => {
+  return (
+    <>
+      <div className="py-14 md:py-28 bg-gray-50">
+        <div className="container">
+          {/* heading section */}
+          <h1 className="pb-16 tracking-wider text-2xl font-semibold text-dark text-center">
+            {""} Benefícios de Escrever todos os Dias
+          </h1>
+          {/* card section */}
+          <div>
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-14 sm:gap-4">
+              {/* 1st card */}
+              <div className="text-center flex justify-center items-center flex-col gap-1 sm:gap-2 px-2">
+                <p className="text-xl text-primary font-semibold">
+                  Redução da Ansiedade:
+                </p>
+                <p className="text-dark/70 font-semibold">
+                  A escrita ajuda a atenuar a ansiedade, pois ao colocar as
+                  palavras no papel, você se concentra nessa função, o que ajuda
+                  a abrandar o ritmo cardíaco e a regular a respiração.
+                </p>
+                <p className="text-5xl rotate-90 text-primary text-center translate-x-5">
+                  ...
+                </p>
+                <FaHeartbeat className="text-5xl text-primary" />
+              </div>
+
+              {/* 2st card */}
+              <div className="text-center flex justify-center items-center flex-col gap-1 px-3">
+                <FaKissWinkHeart className="text-5xl text-secondary" />{" "}
+                <p className="text-5xl rotate-90 text-secondary text-center translate-x-5">
+                  ...
+                </p>
+                <p className="text-xl text-secondary font-semibold">
+                  Melhora da comunicação:
+                </p>
+                <p className="text-dark/70 font-semibold">
+                  A escrita melhora a comunicação, pois ajuda a formular
+                  pensamentos mais complexos e a transmiti-los aos outros com
+                  maior facilidade.
+                </p>
+              </div>
+              {/* 3st card */}
+              <div className="text-center flex justify-center items-center flex-col gap-1 sm:gap-2 px-2">
+                <p className="text-xl text-primary font-semibold">
+                  Promoção da criatividade:
+                </p>
+                <p className="text-dark/70 font-semibold">
+                  A escrita promove a criatividade, pois está associada à
+                  capacidade de resolução de problemas, à procura ativa de
+                  ideias e das relações entre elas, e à materialização de ideias
+                  que surgiram na imaginação.
+                </p>
+                <p className="text-5xl rotate-90 text-primary text-center translate-x-5">
+                  ...
+                </p>
+                <FaLightbulb className="text-5xl text-primary" />
+              </div>
+
+              {/* 4st card */}
+              <div className="text-center flex justify-center items-center flex-col gap-1 px-3">
+                <FaGift className="text-5xl text-secondary" />{" "}
+                <p className="text-5xl rotate-90 text-secondary text-center translate-x-5">
+                  ...
+                </p>
+                <p className="text-xl text-secondary font-semibold">
+                  Aumento da felicidade e da atenção plena:
+                </p>
+                <p className="text-dark/70 font-semibold">
+                  Escrever sobre coisas que te fizeram feliz permite desfrutar
+                  do que você tem, em vez de se concentrar no que lhe falta.
+                  Além disso, essa prática permite apreciar melhor o momento
+                  presente.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Benefits;
