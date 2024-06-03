517bd9aacbeab4e435863da6f8f81811598af928
Janaina <jmtannus@gmail.com>
2024-06-03 09:06:45 -0300

    login button section

--- frontend/src/components/PopUp/PopUp.jsx
+++ frontend/src/components/PopUp/PopUp.jsx
@@ -2,41 +2,63 @@ import React from "react";
 import { IoCloseOutline } from "react-icons/io5";
 
 const PopUp = ({ showPopup, setShowPopup }) => {
-    return (
-      <>
-        {!showPopup && (
-          <div>
-            <div className="h-screen w-screen fixed top-0 left-0
-            bg-black/50 z-50 backdrop-blur-sm">
-                <div className="fixed top-1/2 left-1/2 
+  return (
+    <>
+      {!showPopup && (
+        <div>
+          <div
+            className="h-screen w-screen fixed top-0 left-0
+            bg-black/50 z-50 backdrop-blur-sm"
+          >
+            <div
+              className="fixed top-1/2 left-1/2 
                 -translate-x-1/2 -translate-y-1/2 p-4
-                 shadow-md bg-white rounded-md duration-200 w-[300px]">
-                    {/* header section */}
-                    <div className="flex items-center justify-between">
-                        <div>
-                            <h1 className="text-2xl font-bold text-dark">Login</h1>
-                        </div>
-                        <div>
-                            <IoCloseOutline className="text-2xl cursor-pointer font-bold"
-                                onClick={() => setShowPopup(false)}
-                            />
-                        </div>
-                    </div>
+                 shadow-md bg-white rounded-md duration-200 w-[300px]"
+            >
+              {/* header section */}
+              <div className="flex items-center justify-between">
+                <div>
+                  <h1 className="text-2xl font-bold text-dark">Login</h1>
+                </div>
+                <div>
+                  <IoCloseOutline
+                    className="text-2xl cursor-pointer font-bold"
+                    onClick={() => setShowPopup(false)}
+                  />
+                </div>
+              </div>
 
-                    {/* login form section */}
-                    <div className="mt-4">
-                        <input type="email" placeholder=" Entre com seu e-mail" className="w-full rounded-md border border-gray-300 dark:border-gray-500 px-2 py-1 mb-4"/>
-                        <input type="password" placeholder=" ******** " className="w-full rounded-md border border-gray-300 dark:border-gray-500 px-2 py-1 mb-4"/>
+              {/* login form section */}
+              <div className="mt-4">
+                <input
+                  type="email"
+                  placeholder=" Entre com seu e-mail"
+                  className="w-full rounded-md border border-gray-300 dark:border-gray-500 px-2 py-1 mb-4"
+                />
+                <input
+                  type="password"
+                  placeholder=" * * * * * "
+                  className="w-full rounded-md border border-gray-300 dark:border-gray-500 px-2 py-1 mb-4"
+                />
 
-                        {/* login button section */}
-                    
-                    </div>
-                 </div>              
+                {/* login button section */}
+                <div>
+                  <button
+                    className="w-full bg-primary text-white py-2 rounded-md"
+                    onClick={() => setShowPopup(false)}
+                  >
+                    Entrar
+                  </button>
+                </div>
+
+                {/* social login */}
+              </div>
             </div>
           </div>
-        )}
-      </>
-    );
-  };
+        </div>
+      )}
+    </>
+  );
+};
 
-  export default PopUp;
+export default PopUp;
--- frontend/src/components/Shared/PrimaryButton.jsx
+++ frontend/src/components/Shared/PrimaryButton.jsx
@@ -5,7 +5,7 @@ const PrimaryButton = () => {
   return (
     <>
       <div className="flex items-center group">
-        <button className="bg-primary h-[40px] text-white px-3 py-2">
+        <button className="bg-primary h-[40px] text-white px-3 py-2 rounded-md">
           O que fez você feliz?
         </button>
         <FaArrowRight className="inline-block group-hover:!translate-x-2 duration-200 p-2 text-base h-[40px] w-[40px] bg-primaryDark text-white" />
