31b322b599e4aa1bbc309e86288149e8954702d5
Janaina <jmtannus@gmail.com>
2024-06-03 08:14:39 -0300

    find bugs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./components/Navbar/Navbar.jsx";
+import Navbar from "./components/Navbar/Navbar";
 import Banner from "./components/Banner/Banner.jsx";
 import Hero from "./components/Hero/Hero.jsx";
 import Footer from "./components/Footer/Footer.jsx";
--- frontend/src/components/PopUp/PopUp.jsx
+++ frontend/src/components/PopUp/PopUp.jsx
@@ -1,34 +1,13 @@
-import React from "react";
-import { IoCloseOutline } from "react-icons/io5";
-
 const PopUp = ({ showPopup, setShowPopup }) => {
-  return (
-    <>
-      {!showPopup && (
-        <div>
-          <div className="h-screen w-screen fixed top-0 left-0 bg-black/50 z-50 backdrop-blur-sm">
-            <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-4 
-            shadow-md bg-white rounded-md duration-200 w-[300px]">
-            
-              {/* header section */}
-              <div className="flex items-center justify-between  ">
-                <div>
-                  <h1 className="text-2xl font-bold text-dark">Login</h1>
-                </div>
-              </div>
-              <div>
-                <IoCloseOutline
-                  className="text-2xl cursor-pointer"
-                  onClick={() => setOrderPopup(false)}
-                />
-              </div>
+    return (
+      <>
+        {!showPopup && (
+          <div>
+            <div className="h-screen w-screen fixed top-0 left-0 bg-black/50 z-50 backdrop-blur-sm">
+              hello
             </div>
-            {/* login form section */}
           </div>
-        </div>
-      )}
-    </>
-  );
-};
-
-export default PopUp;
+        )}
+      </>
+    );
+  };
